66e16b01b509f53743c195e41117ee6cccbce6fe
Edward <edward.stevens@qa.com>
2018-06-01 13:25:33 +0100

    Added Select/Dropdown for Time Period

--- src/main/app/src/dashboard/Dashboard.js
+++ src/main/app/src/dashboard/Dashboard.js
@@ -1,9 +1,10 @@
 import React, {Component, Fragment} from 'react';
-import {Divider, Message} from "semantic-ui-react";
+import {Divider, Form, Message} from "semantic-ui-react";
 import TopicSelectComponent from "./TopicSelectComponent";
 import {TOPIC_OPTIONS_ENUM} from "../utils/Constants";
 import {connect} from "react-redux";
-import DashboardGeneric from "./DashboardGeneric";
+import DashboardData from "./DashboardGeneric";
+import TimePeriodSelectComponent from "./TimePeriodSelectComponent";
 
 const mapStateToProps = state => {
   return {
@@ -37,9 +38,20 @@ class DashboardRedux extends Component {
 
     return (
       <Fragment>
-        <TopicSelectComponent/>
+        <Form>
+          <Form.Group>
+            <TopicSelectComponent/>
+            <TimePeriodSelectComponent/>
+            <Message visible info>
+              <Message.Header>PROOF OF CONCEPT</Message.Header>
+              <p>
+                This Dashboard is a <strong>proof of concept</strong> and as such is <em>not</em> the final product.
+              </p>
+            </Message>
+          </Form.Group>
+        </Form>
         <Divider/>
-        <DashboardGeneric topic={topic}/>
+        <DashboardData topic={topic}/>
         <Message>
           The information on this page is refreshed every hour.
         </Message>
--- src/main/app/src/redux/reducers/index.js
+++ src/main/app/src/redux/reducers/index.js
@@ -1,13 +1,16 @@
-import {SET_SELECT_TOPIC,} from "../constants/action-types";
+import {SET_SELECT_TIME_PERIOD, SET_SELECT_TOPIC,} from "../constants/action-types";
 
 const initialState = {
-  topic: ''
+  topic: '',
+  timePeriod: ''
 };
 
 const rootReducer = (state = initialState, action) => {
   switch (action.type) {
     case SET_SELECT_TOPIC:
       return {...state, topic: action.payload};
+    case SET_SELECT_TIME_PERIOD:
+      return {...state, timePeriod: action.payload};
     default:
       return state;
   }
