805a101f9f98d6aee939f8458c1e85713d3f1885
Chris Hardman <christopher.hardman@ons.gov.uk>
2018-06-05 12:22:22 +0100

    added some structure for months quaters and years also mades changes based on code review from simon

--- src/main/java/uk/gov/ons/lerp/poc/endpoint/DashboardEndpoint.java
+++ src/main/java/uk/gov/ons/lerp/poc/endpoint/DashboardEndpoint.java
@@ -1,64 +1,34 @@
 package uk.gov.ons.lerp.poc.endpoint;
 
-import java.io.File;
-import java.io.IOException;
-
 import javax.servlet.http.HttpServletResponse;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.fasterxml.jackson.core.JsonParseException;
-import com.fasterxml.jackson.databind.JsonMappingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
-
+import uk.gov.ons.lerp.poc.service.impl.DashboardServiceImpl;
 import uk.gov.ons.lerp.poc.representation.DashboardData;
-import uk.gov.ons.lerp.poc.service.DashboardService;
 
 @RestController
 public class DashboardEndpoint {
-
-  @Autowired
-  private DashboardService dashBoardService;
+  
+  @Value("${file.directory.birth}")
+  private String fileLoactionBirth;
+  
+  @Value("${file.directory.death}")
+  private String fileLoactionDeath;
+  
+  DashboardServiceImpl service;
   
   @RequestMapping(value = "/ledr-dashboard-poc/topic/{topic}/{period}", method = RequestMethod.GET)
-  public ResponseEntity<DashboardData> queryDB(@PathVariable("topic") final String topic,
+  public ResponseEntity<DashboardData> getDashboardData(@PathVariable("topic") final String topic,
                                                @PathVariable("period") final String period, HttpServletResponse response){
 
-	DashboardData dashboardData = new DashboardData();
-	
-	ObjectMapper mapper = new ObjectMapper();
-	  
-	switch (topic){
-	case "births":
-		try {
-			dashboardData = mapper.readValue(new File("D:\\BirthDashboardData"+period+".json"), DashboardData.class);
-		} catch (JsonParseException e1) {
-			e1.printStackTrace();
-		} catch (JsonMappingException e1) {
-			e1.printStackTrace();
-		} catch (IOException e1) {
-			e1.printStackTrace();
-		}
-      break;
-	case "deaths":
-		try {
-			dashboardData = mapper.readValue(new File("D:\\DeathDashboardData"+period+".json"), DashboardData.class);
-		} catch (JsonParseException e) {
-			e.printStackTrace();
-		} catch (JsonMappingException e) {
-			e.printStackTrace();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-	  break;
-	}
     response.setHeader("Access-Control-Allow-Origin","*");
-    return ResponseEntity.ok(dashboardData);
+    return ResponseEntity.ok(service.retrieveDashboardData(topic,period));
 
   }
 	
--- src/main/java/uk/gov/ons/lerp/poc/service/DashboardService.java
+++ src/main/java/uk/gov/ons/lerp/poc/service/DashboardService.java
@@ -3,11 +3,23 @@ package uk.gov.ons.lerp.poc.service;
 import java.util.Date;
 import java.util.List;
 
+import uk.gov.ons.lerp.poc.representation.DashboardData;
+import uk.gov.ons.lerp.poc.representation.TimePeriod;
+
 public interface DashboardService {
 
-  void callSqlStatments();
-  void findBirthsDashboardData(String week);
-  void findDeathsDashboardData(String week);
-  List<Date> findWeekRange(String week);
+	
+  DashboardData retrieveDashboardData(final String topic, final String period);
+  void callWeeklySqlStatments();
+  void callMonthlySqlStatments();
+  void callQuartlySqlStatments();
+  void callYearlySqlStatments();
+  void findBirthsDashboardData(TimePeriod period);
+  void findDeathsDashboardData(TimePeriod period);
+  List<Date> findPeriodRange(TimePeriod period);
+  List<Date> findWeekRange(TimePeriod week);
+  List<Date> findMonthRange(TimePeriod month);
+  List<Date> findQuarterRange(TimePeriod quarter);
+  List<Date> findYearRange(TimePeriod year);
 	 
 }
