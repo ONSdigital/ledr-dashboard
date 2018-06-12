186e6b612ac6d347f96cff78c82dbdbbe990015d
Edward <edward.stevens@qa.com>
2018-06-12 11:47:35 +0100

    Removed unused reference to Login

--- src/main/app/src/Main.js
+++ src/main/app/src/Main.js
@@ -2,7 +2,6 @@ import React, {Component} from 'react'
 import {Route, Switch} from 'react-router-dom'
 import {ROUTER_PATH} from "./utils/Constants";
 import Dashboard from "./dashboard/Dashboard";
-import Login from "./Login";
 
 // The Main component renders one of the provided
 // Routes (provided that one matches). Routes will match any
@@ -16,9 +15,6 @@ class Main extends Component {
       <Switch>
         <Route exact path={ROUTER_PATH.DASHBOARD} render={(props) => {
           return <Dashboard {...props}/>
-        }}/>
-        <Route exact path={ROUTER_PATH.LOGIN} render={(props) => {
-          return <Login {...props}/>
         }}/>
       </Switch>
     )
