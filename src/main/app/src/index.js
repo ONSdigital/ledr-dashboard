cfb368f3d0038625c967701876ef50f1628875ae
Edward <edward.stevens@qa.com>
2018-06-12 11:58:41 +0100

    Removed service worker and basename

--- src/main/app/src/index.js
+++ src/main/app/src/index.js
@@ -3,11 +3,9 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import 'semantic-ui-css/semantic.min.css';
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
 import {BrowserRouter} from "react-router-dom";
 import {Provider} from "react-redux";
 import store from './redux/store/index';
-import {ROUTER_PATH} from "./utils/Constants";
 
 //Dev Tool to detect needless component renders
 // noinspection JSUnresolvedVariable
@@ -17,7 +15,6 @@ import {ROUTER_PATH} from "./utils/Constants";
   whyDidYouUpdate(React)
 }*/
 
-ReactDOM.render(<Provider store={store}><BrowserRouter basename={ROUTER_PATH.BASE}>
+ReactDOM.render(<Provider store={store}><BrowserRouter>
   <App/>
 </BrowserRouter></Provider>, document.getElementById('root'));
-registerServiceWorker();
