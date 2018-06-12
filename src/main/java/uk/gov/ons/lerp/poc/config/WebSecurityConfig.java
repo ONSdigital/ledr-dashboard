ff19857180a3a9f92438bd9155e3ac169b730b6b
Edward <edward.stevens@qa.com>
2018-06-12 14:41:45 +0100

    Added set 'alwaysUse' to true

--- src/main/java/uk/gov/ons/lerp/poc/config/WebSecurityConfig.java
+++ src/main/java/uk/gov/ons/lerp/poc/config/WebSecurityConfig.java
@@ -28,7 +28,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
       .and()
       .formLogin()
       .loginPage("/login")
-      .defaultSuccessUrl("/", true)
+      .defaultSuccessUrl("/")
       .permitAll()
       .and()
       .logout()
