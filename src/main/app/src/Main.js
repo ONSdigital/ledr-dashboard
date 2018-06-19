4617391d7c8f290cd1d42d6be6eefbf25ee4bca3
Edward <edward.stevens@qa.com>
2018-06-19 11:26:20 +0100

    Removed unnecessary comment

--- src/main/app/src/Main.js
+++ src/main/app/src/Main.js
@@ -3,10 +3,6 @@ import {Route, Switch} from 'react-router-dom'
 import {ROUTER_PATH} from "./utils/Constants";
 import Dashboard from "./dashboard/Dashboard";
 
-// The Main component renders one of the provided
-// Routes (provided that one matches). Routes will match any
-// pathname that starts the name e.g. /jobApplicationNew.
-// The / route will only match when the pathname is exactly the string "/"
 class Main extends Component {
 
   render() {
