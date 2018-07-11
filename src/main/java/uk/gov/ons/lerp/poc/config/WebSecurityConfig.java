e1416501898f145fc6c6b50194a9be4091cf5bc7
Edward <edward.stevens@qa.com>
2018-07-11 14:40:09 +0100

    Fixed authentication/login with Active Directory Added Config classes for sensitive data (to be taken from application.yml not in git)

--- src/main/java/uk/gov/ons/lerp/poc/config/WebSecurityConfig.java
+++ src/main/java/uk/gov/ons/lerp/poc/config/WebSecurityConfig.java
@@ -1,17 +1,21 @@
 package uk.gov.ons.lerp.poc.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.crypto.password.LdapShaPasswordEncoder;
+import org.springframework.security.ldap.authentication.ad.ActiveDirectoryLdapAuthenticationProvider;
 
 @Configuration
 @EnableWebSecurity
 @ComponentScan("uk.gov.ons.lerp.poc")
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+  @Autowired
+  private AppConfig appConfig;
 
   @Override
   protected void configure(HttpSecurity http) throws Exception {
@@ -32,22 +36,24 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
       .permitAll()
       .and()
       .logout()
+      .logoutUrl("/logout")
+      .logoutSuccessUrl("/login")
+      .clearAuthentication(true)
+      .deleteCookies("JSESSIONID")
       .permitAll()
       .and();
   }
 
   @Override
-  public void configure(AuthenticationManagerBuilder auth) throws Exception {
-    auth
-      .ldapAuthentication()
-      .userDnPatterns("uid={0},ou=people")
-      .groupSearchBase("ou=groups")
-      .contextSource()
-      .url("ldap://localhost:8389/dc=springframework,dc=org")
-      .and()
-      .passwordCompare()
-      .passwordEncoder(new LdapShaPasswordEncoder())
-      .passwordAttribute("userPassword");
+  public void configure(AuthenticationManagerBuilder auth) {
+    ActiveDirectoryLdapAuthenticationProvider provider = new ActiveDirectoryLdapAuthenticationProvider(
+      appConfig.getLdap().getDomain(),
+      appConfig.getLdap().getUrl(),
+      appConfig.getLdap().getRootdn());
+    provider.setSearchFilter(appConfig.getLdap().getSearchfilter());
+    provider.setConvertSubErrorCodesToExceptions(true);
+    provider.setUseAuthenticationRequestCredentials(true);
+    auth.authenticationProvider(provider);
   }
 
 }
