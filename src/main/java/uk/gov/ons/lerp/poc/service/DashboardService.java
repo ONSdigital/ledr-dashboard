caba31707ae4a64663ccbf6a1b6be85dcefb25e5
martinhumphrey <martinhumphrey@172.29.59.80>
2018-07-30 15:37:25 +0100

    Refactor service interface method names.

--- src/main/java/uk/gov/ons/lerp/poc/service/DashboardService.java
+++ src/main/java/uk/gov/ons/lerp/poc/service/DashboardService.java
@@ -3,10 +3,34 @@ package uk.gov.ons.lerp.poc.service;
 import uk.gov.ons.lerp.poc.domain.RecordSummary;
 import uk.gov.ons.lerp.poc.exception.CannotRetrieveDashboardData;
 
+/**
+ *  Service responsible for dealing with Management Information requests
+ *
+ */
 public interface DashboardService {
+  
+  /**
+   * Retrieve Birth summary data for specified period.
+   * @param period
+   * @return RecordSummary for birth data
+   * @throws CannotRetrieveDashboardData
+   */
+  RecordSummary retrieveBirthSummaryData(final String period) throws CannotRetrieveDashboardData;
 
-  RecordSummary retrieveBirthDashboardData(final String period) throws CannotRetrieveDashboardData;
-  RecordSummary retrieveDeathDashboardData(final String period) throws CannotRetrieveDashboardData;
-  RecordSummary retrieveDeathOccupationData(final String period) throws CannotRetrieveDashboardData;
+  /**
+   * Retrieve Death summary data for specified period.
+   * @param period
+   * @return RecordSummary for death data
+   * @throws CannotRetrieveDashboardData
+   */
+  RecordSummary retrieveDeathSummaryData(final String period) throws CannotRetrieveDashboardData;
+
+  /**
+   * Retrieve Death Cause Coding detail data for specified period.
+   * @param period
+   * @return CauseDetail for Death data
+   * @throws CannotRetrieveDashboardData
+   */
+  RecordSummary retrieveDeathCauseDetailData(final String period) throws CannotRetrieveDashboardData;
 	 
 }
